667133eef3c20eff29265dec66e561cc00537d2c
Mithu Roy <roy.mithu66@gmail.com>
2022-10-26 14:42:56 +0530

    A commit for fun again

--- app/src/main/java/com/mithuroy/styleremote/MainActivity.kt
+++ app/src/main/java/com/mithuroy/styleremote/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // This is to check git
+        // Another comment
     }
 }
